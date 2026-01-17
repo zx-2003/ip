5dd7562e49cace3a4c7892ba93ef4e7bcc3f2139
Tan Zhuo Xuan <tanzhuoxuan2@gmail.com>
2026-01-17 23:54:07 +0800

    Level 1. Echo -> done

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Bird {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Bird");
-        System.out.println("What can I do for you?\n");
+        System.out.println("    ____________________________________________");
+        System.out.println("    Hello! I'm Bird");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            String input = scanner.nextLine();
+            System.out.println("    ____________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                running = false;
+            } else {
+                System.out.println("    " + input);
+            }
+            System.out.println("    ____________________________________________");
+        }
     }
 }
 

dabf24656cd27a77b93494a915facfba1fa4cbf1
Tan Zhuo Xuan <tanzhuoxuan2@gmail.com>
2026-01-17 23:17:43 +0800

    Level 0. Rename, Greet, Exit -> done

---
+++ src/main/java/Bird.java
@@ -0,0 +1,9 @@
+public class Bird {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bird");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+

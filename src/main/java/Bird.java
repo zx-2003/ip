0ca5f2fde2aa28dec0e6eaa172bcc52bff77b6b5
Tan Zhuo Xuan <tanzhuoxuan2@gmail.com>
2026-01-18 00:19:21 +0800

    Level 2. Add, List

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bird {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        ArrayList<String> tasks = new ArrayList<>();
+
         System.out.println("    ____________________________________________");
         System.out.println("    Hello! I'm Bird");
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________");
 
-        Scanner scanner = new Scanner(System.in);
-        boolean running = true;
-
         while (running) {
             String input = scanner.nextLine();
             System.out.println("    ____________________________________________");
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                // first need to find a way to increment, second write item next to it
+                int taskItemNumber = 1;
+                for (String task : tasks) {
+                    System.out.println("    " + taskItemNumber + ". " + task);
+                    taskItemNumber = taskItemNumber + 1;
+                }
+            } else if (input.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
                 running = false;
             } else {
-                System.out.println("    " + input);
+                tasks.add(input);
+                System.out.println("    added: " + input);
             }
             System.out.println("    ____________________________________________");
         }

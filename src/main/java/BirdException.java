5ebe0b6773334aa115ccabd0fd837885b676d703
Tan Zhuo Xuan <tanzhuoxuan2@gmail.com>
2026-01-23 13:33:25 +0800

    i added a fullstop to the exception handler

--- src/main/java/BirdException.java
+++ src/main/java/BirdException.java
@@ -1,7 +1,7 @@
 public class BirdException extends Exception {
 
     public BirdException() {
-        super("Sorry I don't understand what is that supposed to mean");
+        super("Sorry I don't understand what is that supposed to mean.");
     }
 
     public BirdException(String message) {

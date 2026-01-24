7a92d6265cd9c971a0d9e2f858ced048bf57191a
Tan Zhuo Xuan <tanzhuoxuan2@gmail.com>
2026-01-24 22:16:43 +0800

    Add javaDoc documentation
    added javaDoc documentation to most of the .java files

--- src/main/java/bird/Deadline.java
+++ src/main/java/bird/Deadline.java
@@ -3,15 +3,27 @@ package bird;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a deadline task.
+ */
 public class Deadline extends Task {
 
     protected LocalDateTime date;
 
+    /**
+     * Constructor for the deadline task.
+     * @param description a brief description of the task.
+     * @param date the date and time of the event given in yyyy-MM-dd HHmm format.
+     */
     public Deadline(String description, LocalDateTime date) {
         super(description);
         this.date = date;
     }
 
+    /**
+     * Provides a string for the deadline to be cached locally.
+     * @return the String format for the deadline that is saved in the storage.
+     */
     @Override
     public String toFileString() {
         return "D|" + (this.isDone? "1": "0") + "|" + this.description + "|" + this.date;
--- src/main/java/bird/ToDos.java
+++ src/main/java/bird/ToDos.java
@@ -1,7 +1,14 @@
 package bird;
 
+/**
+ * Represents a ToDo task
+ */
 public class ToDos extends Task {
 
+    /**
+     * Constructor for the ToDo task
+     * @param description a brief description of the task
+     */
     public ToDos(String description) {
         super(description);
     }
